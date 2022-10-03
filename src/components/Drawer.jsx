2804c5732bd534f8d7e630da369f356221e76f70
Prince Codes <princecodes247@gmail.com>
2022-10-03 14:55:54 +0100

    update sidemenu

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,27 +1,62 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function Drawer({ children, links, currentWorkspace }) {
+function Drawer({ children, currentWorkspace }) {
+  // Check the current url from react-router-dom useLocation hook
+  const location = useLocation();
+  const isActive = (path) => {
+    console.log(location.pathname);
+    if (location.pathname === path) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   currentWorkspace = currentWorkspace || "";
-  links = links || [];
+  const links = [
+    {
+      name: "Overview",
+      path: "/",
+    },
+    {
+      name: "Projects",
+      path: "/projects",
+    },
+    {
+      name: "Settings",
+      path: `/settings`,
+    },
+    {
+      name: "Help",
+      path: "/help",
+    },
+  ];
   return (
     <div className="drawer drawer-mobile">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content p-8">
         {/* <!-- Page content here --> */}
+        momo
         {children}
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay">
           {currentWorkspace}
         </label>
-        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        {/* <div className="bg-base-100 text-base-content inline md:flex flex-col gap-4"> */}
+        <ul className="menu bg-base-100  p-4 overflow-y-auto gap-1 w-80 ">
+          <button className="btn btn-primary  my-12">Create Project</button>
           {/* <!-- Sidebar content here --> */}
-          {links.map((link) => {
-            <li>
-              <Link to={`/form/${link?._id}`}>{link?.name}</Link>
-            </li>;
-          })}
+          {links.map((link) => (
+            <li
+              className={`rounded ${
+                isActive(link?.path) ? "bg-blue-100" : "bg-transparent"
+              }`}
+            >
+              <Link to={`${link?.path}`}>{link?.name}</Link>
+            </li>
+          ))}
         </ul>
+        {/* </div> */}
       </div>
     </div>
   );
--- src/components/Header/indexold.jsx
+++ src/components/Header/indexold.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Header() {
   return (
-    <div className="navbar px-6 flex bg-base-100">
+    <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
           <label
@@ -41,17 +41,31 @@ function Header() {
           </ul>
         </div>
       </div>
-      <div className="navbar-center hidden w-full md:flex flex-1">
-        <div className="searchbar border w-[38vw] flex items-center p-1 px-2 rounded">
-          <input
-            type="text"
-            className="p-4 py-3 flex-1 bg-transparent outline-none"
-            placeholder="Search here..."
-          />
-          <button className="btn btn-primary rounded-lg px-8">
+      <div className="navbar-center">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
+              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -60,42 +74,12 @@ function Header() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />
             </svg>
-          </button>
-        </div>
-
-        <ul className="flex gap-4 items-center text-sm ml-8">
-          <li>HOME</li>
-          <li>ABOUT</li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <ul className="menu menu-horizontal p-0">
-          <li tabIndex={0}>
-            <a>
-              O
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="-left-12 p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
       </div>
     </div>
   );
