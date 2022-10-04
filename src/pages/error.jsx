b55f45af7688819e3c775bd6b7352f0a47177a73
Prince Codes <princecodes247@gmail.com>
2022-10-04 11:35:48 +0100

    style: update error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Header } from "../components";
 
 function Error() {
   return (
     <>
-      <section>
-        <h1>Something went wrong</h1>
+      <Header />
+      <section className="flex justify-center items-center h-screen flex-col">
+        <h1 className="text-7xl font-bold md:text-9xl">404</h1>
+        <h1 className="text-3xl">Something went wrong</h1>
         <p>This link is broken or does not exist</p>
-        <button>Return to home</button>
+        <Link to={"/"} className="btn btn-primary mt-8">
+          Return to home
+        </Link>
       </section>
     </>
   );
