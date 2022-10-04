24ea2a52126fa08c2295cb2fdec3b7b9157aeba4
Prince Codes <princecodes247@gmail.com>
2022-10-04 13:29:51 +0100

    feat: Add hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,20 @@
+import useAsync from "./useAsync";
+import { useAuth, ProvideAuth } from "./useAuth";
+import useRequireAuth from "./useRequireAuth";
+import useDebounce from "./useDebounce";
+import useLocalStorage from "./useLocalStorage";
+import useLockBodyScroll from "./useLockBodyScroll";
+import useOnClickOutside from "./useOnClickOutside";
+import useOnScreen from "./useOnScreen";
+
+export {
+  useAsync,
+  useAuth,
+  ProvideAuth,
+  useRequireAuth,
+  useDebounce,
+  useLocalStorage,
+  useLockBodyScroll,
+  useOnClickOutside,
+  useOnScreen,
+};
---
+++ src/hooks/useRequireAuth.js
@@ -0,0 +1,14 @@
+export default function useRequireAuth(redirectUrl = "/signup") {
+  const auth = useAuth();
+  const router = useRouter();
+
+  // If auth.user is false that means we're not
+  // logged in and should redirect.
+  useEffect(() => {
+    if (auth.user === false) {
+      router.push(redirectUrl);
+    }
+  }, [auth, router]);
+
+  return auth;
+}
