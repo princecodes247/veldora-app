9d0bada8acfff5eabf0fb5c647c9ad67522cb677
Prince Codes <princecodes247@gmail.com>
2022-10-04 11:35:57 +0100

    feat: update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -27,21 +27,46 @@ function Header() {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box"
           >
             <li>
-              <a>Homepage</a>
+              <div className="searchbar border flex items-center p-1 px-2 rounded">
+                <input
+                  type="text"
+                  className="p-4 py-3 flex-1 bg-transparent outline-none"
+                  placeholder="Search here..."
+                />
+                <button className="btn btn-primary rounded-lg px-8">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+              </div>
             </li>
-            <li>
-              <a>Portfolio</a>
+            <li className="mt-6">
+              <a>Home</a>
             </li>
             <li>
               <a>About</a>
             </li>
+            <li>
+              <a>Docs</a>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="navbar-center hidden w-full md:flex flex-1">
+      <div className="navbar-center hidden w-full lg:flex flex-1">
         <div className="searchbar border w-[38vw] flex items-center p-1 px-2 rounded">
           <input
             type="text"
@@ -69,6 +94,7 @@ function Header() {
         <ul className="flex gap-4 items-center text-sm ml-8">
           <li>HOME</li>
           <li>ABOUT</li>
+          <li>DOCS</li>
         </ul>
       </div>
       <div className="navbar-end">
