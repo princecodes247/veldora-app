4f6d2c9ec2cdbd7e5bd7d4f742bcdbd3fe0d06cc
Prince Codes <princecodes247@gmail.com>
2022-10-06 15:39:31 +0100

    style: change drawer layout to slick's design

--- src/components/layouts/DashboardLayout.jsx
+++ src/components/layouts/DashboardLayout.jsx
@@ -5,10 +5,10 @@ import Header from "../Header";
 function DashboardLayout({ children }) {
   return (
     <>
-      <Header />
       <div className="drawer drawer-mobile">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content p-8">
+        <div className="drawer-content p-8 pt-0">
+          <Header />
           {/* <!-- Page content here --> */}
           {children}
         </div>
