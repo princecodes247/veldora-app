d03224dd726a20dd4fad895765a9a52b83e79f48
Prince Codes <princecodes247@gmail.com>
2022-10-24 13:39:57 +0100

    fix: tabs spanning infinitely

--- src/components/layouts/FormLayout.jsx
+++ src/components/layouts/FormLayout.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
-import Header from "../Header";
-import Drawer from "../Drawer";
 import DashboardLayout from "./DashboardLayout";
 import useCurrentPage from "../../hooks/useCurrentPage";
 
-function Tabs(props) {
+function Tabs() {
   const currPage = useCurrentPage(3);
   const { projectId } = useParams();
   useEffect(() => {
@@ -14,7 +12,7 @@ function Tabs(props) {
   }, []);
 
   return (
-    <div className="tabs tabs-boxed my-4">
+    <div className="tabs tabs-boxed w-fit my-4">
       <Link
         to={`/projects/${projectId}`}
         className={`tab  ${currPage === undefined && "tab-active"}`}
