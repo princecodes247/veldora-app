ff95a63efb2d973066acffd74814b6e9eced9dd1
Praisecodes <cmancoder@gmail.com>
2022-10-25 21:11:58 +0100

    [Update] Creating Schema component

--- src/components/modals/CreateProjectModal.jsx
+++ src/components/modals/CreateProjectModal.jsx
@@ -1,26 +1,17 @@
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
-import { CgLoadbar } from "react-icons/cg";
-import { IoIosArrowDown } from "react-icons/io";
 import { useState } from "react";
+import Schema from "./components/schema";
 
 export default function CreateProjectModal() {
   const [nextPage, setNextPage] = useState(false);
-  const [usernameOpen, setusernameOpen] = useState(false);
   const [projectName, setProjectName] = useState("");
   const [description, setDescription] = useState("");
   const [tag, setTag] = useState("");
   const [ProjectType, setType] = useState("Registration Form");
-  //Practice
-  // const [firstSectionDetails, setFSecDetails] = useState({
-  //   'projectName': "fyy",
-  //   'description': "",
-  //   'tag': "",
-  //   'type': "Registration Form"
-  // });
 
   //Function To Handle Input Changes
-  function StoreData(inputVal, action){
-    switch(action){
+  function StoreData(inputVal, action) {
+    switch (action) {
       case 'ADD_PROJECT_NAME':
         setProjectName(inputVal);
         break;
@@ -66,7 +57,7 @@ export default function CreateProjectModal() {
                   placeholder="Project Name"
                   value={projectName}
                   onChange={
-                    (e)=>{
+                    (e) => {
                       StoreData(e.target.value, "ADD_PROJECT_NAME")
                     }
                   }
@@ -93,7 +84,7 @@ export default function CreateProjectModal() {
               placeholder="Project Description"
               value={description}
               onChange={
-                (e)=>{
+                (e) => {
                   StoreData(e.target.value, "ADD_DESCRIPTION")
                 }
               }
@@ -117,7 +108,7 @@ export default function CreateProjectModal() {
               placeholder="Project Tag"
               value={tag}
               onChange={
-                (e)=>{
+                (e) => {
                   StoreData(e.target.value, "ADD_TAG")
                 }
               }
@@ -154,111 +145,7 @@ export default function CreateProjectModal() {
             </h1>{" "}
             <br /> <hr />
             <br />
-            <div
-              className={`border-gray-400 rounded border py-3 lg:w-3/4 w-full transition duration-500 overflow-hidden ${
-                usernameOpen ? "h-fit" : "h-18"
-              }`}
-            >
-              <div className="px-5 py-3 border-b border-gray-300 flex justify-between">
-                <h2 className="font-bold text-xl">
-                  UserName{" "}
-                  <span className="font-normal text-sm text-gray-600">
-                    required
-                  </span>
-                </h2>
-                <div className="flex">
-                  <i className="text-lg rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200">
-                    <CgLoadbar />
-                  </i>
-                  <i
-                    className={`text-xl font-bold rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200 cursor-pointer ${
-                      usernameOpen ? "rotate-180" : ""
-                    }`}
-                    onClick={() => {
-                      setusernameOpen(!usernameOpen);
-                    }}
-                  >
-                    <IoIosArrowDown />
-                  </i>
-                </div>
-              </div>
-              <div className="flex w-full px-3 py-1 justify-between items-end flex-wrap">
-                <div className="lg:w-sectwo lg:mb-0 mb-1">
-                  <p className="text-gray-900 text-lg ml-4 mb-1">
-                    Filled Name{" "}
-                    <span className="text-sm text-gray-500 font-thin">
-                      required
-                    </span>
-                  </p>
-                  <input
-                    type="text"
-                    className="input w-full border text-gray-800 focus:outline-none text-sm border-gray-300 rounded-sm"
-                    placeholder="UserName"
-                  />
-                </div>
-                <div className="flex w-40 bg-gray-200 p-3 rounded-sm items-center">
-                  <input
-                    type="checkbox"
-                    className="checkbox checkbox-sm rounded-sm no-animation mr-2"
-                  />
-                  <p className="text-sm">Required</p>
-                </div>
-              </div>{" "}
-              <br />
-              <div className="flex w-full px-3 py-1 justify-between items-end flex-wrap">
-                <div className="w-sectwo lg:mb-0 mb-1">
-                  <p className="text-gray-900 text-lg ml-4 mb-1">Filled Type</p>
-                  <select className="input w-full border text-gray-800 focus:outline-none text-sm border-gray-300 rounded-sm">
-                    <option value="Text">Text</option>
-                  </select>
-                </div>
-                <div className="flex w-40 bg-gray-200 p-3 rounded-sm items-center">
-                  <input
-                    type="checkbox"
-                    className="checkbox checkbox-sm rounded-sm no-animation mr-2"
-                  />
-                  <p className="text-sm">Unique</p>
-                </div>
-              </div>{" "}
-              <br />
-              <div className="flex w-full px-3 py-1 justify-between items-end flex-wrap">
-                <div className="lg:w-sectwo lg:mb-0 mb-1">
-                  <p className="text-gray-900 text-lg ml-4 mb-1">
-                    Error Message{" "}
-                    <span className="text-sm text-gray-500 font-thin">
-                      required
-                    </span>
-                  </p>
-                  <input
-                    type="text"
-                    className="input w-full border text-gray-800 focus:outline-none text-sm border-gray-300 rounded-sm"
-                    placeholder="Error Message"
-                  />
-                </div>
-                <select className="flex w-40 bg-gray-200 p-3 focus:outline-none rounded-sm items-center">
-                  <option value="Match filled">Match filled</option>
-                </select>
-              </div>
-            </div>{" "}
-            <br />
-            <div className="border border-gray-400 rounded py-3 lg:w-3/4 w-full">
-              <div className="px-5 flex justify-between">
-                <h2 className="font-bold text-xl">
-                  Password{" "}
-                  <span className="font-normal text-sm text-gray-600">
-                    required
-                  </span>
-                </h2>
-                <div className="flex">
-                  <i className="text-lg rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200">
-                    <CgLoadbar />
-                  </i>
-                  <i className="text-xl font-bold rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200 hover:rotate-180 cursor-pointer">
-                    <IoIosArrowDown />
-                  </i>
-                </div>
-              </div>
-            </div>
+            <Schema />
           </div>
           {/** Second Modal Section End */}
           <br />
@@ -266,9 +153,8 @@ export default function CreateProjectModal() {
           <br />
           <div className="flex">
             <button
-              className={`btn no-animation mr-3 rounded-sm bg-gray-100 border-0 hover:bg-gray-200 text-black text-sm p-0 w-40 font-poppins ${
-                nextPage ? "" : "hidden"
-              }`}
+              className={`btn no-animation mr-3 rounded-sm bg-gray-100 border-0 hover:bg-gray-200 text-black text-sm p-0 w-40 font-poppins ${nextPage ? "" : "hidden"
+                }`}
               onClick={() => {
                 setNextPage(!nextPage);
               }}
@@ -280,11 +166,9 @@ export default function CreateProjectModal() {
             </button>
 
             <button
-              className={`btn no-animation rounded-sm bg-gray-100 border-0 ${
-                !nextPage ? "hover:bg-gray-200" : "hover:bg-gray-100"
-              } text-black text-sm p-0 w-40 font-poppins ${
-                !nextPage ? "" : "hidden"
-              }`}
+              className={`btn no-animation rounded-sm bg-gray-100 border-0 ${!nextPage ? "hover:bg-gray-200" : "hover:bg-gray-100"
+                } text-black text-sm p-0 w-40 font-poppins ${!nextPage ? "" : "hidden"
+                }`}
               onClick={(e) => {
                 setNextPage(!nextPage);
                 console.log(nextPage);
@@ -298,9 +182,8 @@ export default function CreateProjectModal() {
             </button>
 
             <button
-              className={`btn no-animation rounded-sm bg-gray-100 border-0 hover:bg-gray-200 text-black text-sm p-0 w-40 font-poppins ${
-                nextPage ? "" : "hidden"
-              }`}
+              className={`btn no-animation rounded-sm bg-gray-100 border-0 hover:bg-gray-200 text-black text-sm p-0 w-40 font-poppins ${nextPage ? "" : "hidden"
+                }`}
               onClick={(e) => {
                 setNextPage(!nextPage);
                 console.log(nextPage);
---
+++ src/components/modals/components/schema.jsx
@@ -0,0 +1,114 @@
+import { CgLoadbar } from "react-icons/cg";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
+
+export default function Schema(props){
+    const [usernameOpen, setusernameOpen] = useState(false);
+    return (
+        <>
+        <div
+              className={`border-gray-400 rounded border py-3 lg:w-3/4 w-full transition duration-500 overflow-hidden ${usernameOpen ? "h-fit" : "h-18"
+                }`}
+            >
+              <div className="px-5 py-3 border-b border-gray-300 flex justify-between">
+                <h2 className="font-bold text-xl">
+                  UserName{" "}
+                  <span className="font-normal text-sm text-gray-600">
+                    required
+                  </span>
+                </h2>
+                <div className="flex">
+                  <i className="text-lg rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200">
+                    <CgLoadbar />
+                  </i>
+                  <i
+                    className={`text-xl font-bold rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200 cursor-pointer ${usernameOpen ? "rotate-180" : ""
+                      }`}
+                    onClick={() => {
+                      setusernameOpen(!usernameOpen);
+                    }}
+                  >
+                    <IoIosArrowDown />
+                  </i>
+                </div>
+              </div>
+              <div className="flex w-full px-3 py-1 justify-between items-end flex-wrap">
+                <div className="lg:w-sectwo lg:mb-0 mb-1">
+                  <p className="text-gray-900 text-lg ml-4 mb-1">
+                    Filled Name{" "}
+                    <span className="text-sm text-gray-500 font-thin">
+                      required
+                    </span>
+                  </p>
+                  <input
+                    type="text"
+                    className="input w-full border text-gray-800 focus:outline-none text-sm border-gray-300 rounded-sm"
+                    placeholder="UserName"
+                  />
+                </div>
+                <div className="flex w-40 bg-gray-200 p-3 rounded-sm items-center">
+                  <input
+                    type="checkbox"
+                    className="checkbox checkbox-sm rounded-sm no-animation mr-2"
+                  />
+                  <p className="text-sm">Required</p>
+                </div>
+              </div>{" "}
+              <br />
+              <div className="flex w-full px-3 py-1 justify-between items-end flex-wrap">
+                <div className="w-sectwo lg:mb-0 mb-1">
+                  <p className="text-gray-900 text-lg ml-4 mb-1">Filled Type</p>
+                  <select className="input w-full border text-gray-800 focus:outline-none text-sm border-gray-300 rounded-sm">
+                    <option value="Text">Text</option>
+                  </select>
+                </div>
+                <div className="flex w-40 bg-gray-200 p-3 rounded-sm items-center">
+                  <input
+                    type="checkbox"
+                    className="checkbox checkbox-sm rounded-sm no-animation mr-2"
+                  />
+                  <p className="text-sm">Unique</p>
+                </div>
+              </div>{" "}
+              <br />
+              <div className="flex w-full px-3 py-1 justify-between items-end flex-wrap">
+                <div className="lg:w-sectwo lg:mb-0 mb-1">
+                  <p className="text-gray-900 text-lg ml-4 mb-1">
+                    Error Message{" "}
+                    <span className="text-sm text-gray-500 font-thin">
+                      required
+                    </span>
+                  </p>
+                  <input
+                    type="text"
+                    className="input w-full border text-gray-800 focus:outline-none text-sm border-gray-300 rounded-sm"
+                    placeholder="Error Message"
+                  />
+                </div>
+                <select className="flex w-40 bg-gray-200 p-3 focus:outline-none rounded-sm items-center">
+                  <option value="Match filled">Match filled</option>
+                </select>
+              </div>
+            </div>{" "}
+            <br />
+            <div className="border border-gray-400 rounded py-3 lg:w-3/4 w-full">
+              <div className="px-5 flex justify-between">
+                <h2 className="font-bold text-xl">
+                  Password{" "}
+                  <span className="font-normal text-sm text-gray-600">
+                    required
+                  </span>
+                </h2>
+                <div className="flex">
+                  <i className="text-lg rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200">
+                    <CgLoadbar />
+                  </i>
+                  <i className="text-xl font-bold rounded-full p-2 flex justify-center items-center transition duration-200 hover:bg-gray-200 hover:rotate-180 cursor-pointer">
+                    <IoIosArrowDown />
+                  </i>
+                </div>
+              </div>
+            </div>
+        </>
+    )
+}
